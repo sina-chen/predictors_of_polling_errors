1a5ecde3ee1fff1c3fa7cea19c1d0eff3e8929e6
jkortner <jkortner@pm.me>
2020-10-22 16:20:26 +0200

    Update face files

--- us_senate/covariates/us_senate_faces.R
+++ us_senate/covariates/us_senate_faces.R
@@ -7,20 +7,20 @@ library(tidyverse)
 
 # prepare data -------------------------------------------------------------- #
 
-sen_faces <- read.csv('sen_faces.csv')
+sen_faces <- read.csv('us_senator_pictures1998_2018/sen_faces.csv', sep = ",")
 sen_faces <- sen_faces %>% 
   select(candidate, jpg, party)
 
 # classify images ----------------------------------------------------------- #
 
-jpgs <- list.files(path = "portraits")
+jpgs <- list.files(path = "us_senator_pictures1998_2018/portraits")
 d_list <- list()
 pb <- progress_bar$new(total = length(jpgs), show_after = 0)
 for (i in 1:length(jpgs)){
   pb$tick()
-  jpg_path = paste0('portraits/', jpgs[i])
+  jpg_path = paste0('us_senator_pictures1998_2018/portraits/', jpgs[i])
   
-  # https://github.com/ybml/clarifai
+  # https://github.com/jkortner/clarifai
   d <- face2race(jpg_path, api_key)
   d[['jpg']] <- jpgs[i]
   d_list <- c(d_list, d)
@@ -28,7 +28,7 @@ for (i in 1:length(jpgs)){
 }
 
 df <- d_list_to_df(d_list)
-# write.csv(df, 'clarifai_demographics.csv')
+write.csv(df, 'predictors_of_polling_errors/data/senate/clarifai_demographics.csv')
 
 # post-process data (race / gender) ----------------------------------------- #
 
@@ -41,4 +41,4 @@ df <- df %>%
 # merge --------------------------------------------------------------------- #
 
 sen_faces <- merge(x = sen_faces, y = df, by = "jpg", all.x = TRUE)
-# write.csv(sen_faces, 'clarifai_race.csv')
+write.csv(sen_faces, 'predictors_of_polling_errors/data/senate/clarifai_race.csv')
