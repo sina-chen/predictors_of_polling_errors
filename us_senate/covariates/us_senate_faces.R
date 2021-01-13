9bbb88db57f95fce9fe1e1b18faec04851be8db2
jkortner <jkortner@pm.me>
2021-01-13 11:07:21 +0100

    Update Clarifai labels with new workflow

--- us_senate/covariates/us_senate_faces.R
+++ us_senate/covariates/us_senate_faces.R
@@ -14,6 +14,7 @@ sen_faces <- sen_faces %>%
 
 # classify images ----------------------------------------------------------- #
 
+# gender
 # jpgs <- list.files(path = "us_senator_pictures1998_2018/portraits")
 jpgs <- list.files(path = "us_senator_pictures1998_2018/portraits_including_2020")
 d_list <- list()
@@ -24,24 +25,47 @@ for (i in 1:length(jpgs)){
   jpg_path = paste0('us_senator_pictures1998_2018/portraits_including_2020/', jpgs[i])
   
   # https://github.com/jkortner/clarifai
+  d <- face2gender(jpg_path, api_key)
+  d[['jpg']] <- jpgs[i]
+  d_list <- c(d_list, d)
+  
+}
+
+df_gender <- d_list_to_df(d_list)
+write.csv(df_gender, 'predictors_of_polling_errors/data/us_senate/clarifai_gender.csv')
+
+# race
+jpgs <- list.files(path = "us_senator_pictures1998_2018/portraits_including_2020")
+d_list <- list()
+pb <- progress_bar$new(total = length(jpgs), show_after = 0)
+for (i in 1:length(jpgs)){
+  pb$tick()
+  jpg_path = paste0('us_senator_pictures1998_2018/portraits_including_2020/', jpgs[i])
+  
+  # https://github.com/jkortner/clarifai
   d <- face2race(jpg_path, api_key)
   d[['jpg']] <- jpgs[i]
   d_list <- c(d_list, d)
   
 }
 
-df <- d_list_to_df(d_list)
-write.csv(df, 'predictors_of_polling_errors/data/senate/clarifai_demographics.csv')
+df_race <- d_list_to_df(d_list)
+write.csv(df_race, 'predictors_of_polling_errors/data/us_senate/clarifai_race.csv')
 
-# post-process data (race / gender) ----------------------------------------- #
+# post-process data --------------------------------------------------------- #
 
-df$clarifai_gender <- names(df)[4:5][max.col(df[4:5], 'first')]
-df$clarifai_race <- names(df)[6:12][max.col(df[6:12], 'first')]
+df_gender$clarifai_gender <- names(df_gender)[1:2][max.col(df_gender[1:2], 'first')]
+df_race$clarifai_race <- names(df_race)[1:7][max.col(df_race[1:7], 'first')]
 
-df <- df %>% 
-  select(jpg, clarifai_gender, clarifai_race)
+df_gender <- df_gender %>% 
+  select(jpg, clarifai_gender)
+
+df_race <- df_race %>% 
+  select(jpg, clarifai_race)
 
 # merge --------------------------------------------------------------------- #
 
-sen_faces <- merge(x = sen_faces, y = df, by = "jpg", all.x = TRUE)
-write.csv(sen_faces, 'predictors_of_polling_errors/data/senate/clarifai_race.csv')
+sen_faces <- merge(x = sen_faces, y = df_gender, by = "jpg", all.x = TRUE)
+sen_faces <- merge(x = sen_faces, y = df_race, by = "jpg", all.x = TRUE)
+
+write.csv(sen_faces, 'predictors_of_polling_errors/data/us_senate/clarifai_labels.csv')
