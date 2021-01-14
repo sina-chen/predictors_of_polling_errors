6460287731c57a356462437860b077e8ec317911
jkortner <jkortner@pm.me>
2021-01-14 17:10:30 +0100

    Update image labels

--- us_senate/covariates/check_clarifai.R
+++ us_senate/covariates/check_clarifai.R
@@ -3,47 +3,68 @@
 # --------------------------------------------------------------------------- #
 
 library(tidyverse)
+library(ggplot2)
+library(cvms)
 
-sen_faces_clarifai <- read.csv('predictors_of_polling_errors/data/us_senate/clarifai_race.csv')
-# sen_faces_john <- read.csv('data/us_senate/manually_race_gender_john.csv', sep = ';')
+sen_faces_clarifai <- read.csv('predictors_of_polling_errors/data/us_senate/clarifai_labels.csv')
+sen_faces_human <- read.csv('predictors_of_polling_errors/data/us_senate/human_labels.csv', sep = ";")
+
+sen_faces <- merge(x = sen_faces_clarifai, y = sen_faces_human, by = "jpg", all.x = TRUE)
+sen_faces <- sen_faces %>% 
+  select(candidate.x, jpg, party, clarifai_gender, clarifai_race, gender, race)
+sen_faces <- rename(sen_faces, candidate = candidate.x)
+# --------------------------------------------------------------------------- #
+
+# clean: drop candidates not used in subsequent steps
+drop_list <- c( "abby_broyles.jpg", "adrian_perkins.jpg", "alexi_giannoulias.jpg", "allen_waters.jpg", 
+  "ben_sasse.jpg", "bill_hagerty.jpg", "charles_e_summers_jr.jpg", "chris_janicek.jpg", "cynthia_dill.jpg", 
+  "dan_ahlers.jpg", "foster_campbell.jpg", "jean_carnahan.jpg", "jo_rae_perkins.jpg", "jon_ossoff.jpg", 
+  "kelly_loeffler.jpg", "kevin_o'connor.jpg", "mack_mattingly.jpg", "mark_curran.jpg", "mark_kelly.jpg", 
+  "marquita_bradshaw.jpg", "martha_coakley.jpg", "paula_jean_swearengin.jpg", "paulette_jordan.jpg", 
+  "raphael_warnock.jpg", "ricky_dale_harrington.jpg", "shelley_moore_capito.jpg", "zell_miller.jpg")
+
+sen_faces <- sen_faces[!sen_faces$jpg %in% drop_list, ]
 
 # --------------------------------------------------------------------------- #
-sen_faces_clarifai <- sen_faces_clarifai %>%
-  mutate(clarifai_race = replace(clarifai_race, candidate == 'Alan Schlesinger', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Dan Bongino', 'hispanic'),
-         clarifai_race = replace(clarifai_race, candidate == 'Jim Huffman', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Linda Smith', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Lou Barletta', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Philip Giordano', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Rick Lazio', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Robert Lorge', 'hispanic'),
-         clarifai_race = replace(clarifai_race, candidate == 'Barbara Boxer', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Bob Menendez', 'white'), # or hispanic
-         clarifai_race = replace(clarifai_race, candidate == 'Catherine Cortez Masto', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Harold Ford Jr.', 'black'),
-         clarifai_race = replace(clarifai_race, candidate == 'Ken Salazar', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Paul Hodes', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Rick Noriega', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Sam Granato', 'white'),
-         # 2020
-         clarifai_race = replace(clarifai_race, candidate == 'Ben Ray Lujan', 'hispanic'),
-         clarifai_race = replace(clarifai_race, candidate == 'Dan Ahlers', 'white'),
-         clarifai_race = replace(clarifai_race, candidate == 'Lauren Witzke', 'white')) %>%
+
+# gender
+conf_mat_gender <- confusion_matrix(targets = sen_faces$gender,
+                                    predictions = sen_faces$clarifai_gender)
+
+plot_confusion_matrix(conf_mat_gender, add_sums = TRUE, palette = "Greens")
+
+sen_faces[sen_faces$clarifai_gender != sen_faces$gender, ]
+
+# race
+conf_mat_race <- confusion_matrix(targets = sen_faces$race,
+                             predictions = sen_faces$clarifai_race)
+
+plot_confusion_matrix(conf_mat_race, add_sums = TRUE, palette = "Greens")
+
+sen_faces[sen_faces$clarifai_race != sen_faces$race, ]
+
+# --------------------------------------------------------------------------- #
+
+sen_faces_checked <- sen_faces
+sen_faces_checked$gender_checked <- sen_faces$gender
+sen_faces_checked$race_checked <- sen_faces$race
+  
+sen_faces_checked <- sen_faces_checked %>%
+  mutate(race_checked = replace(race_checked, candidate == 'Marco Rubio', 'Cuban'),
+         race_checked = replace(race_checked, candidate == 'Ted Cruz', 'Cuban'),
+         race_checked = replace(race_checked, candidate == 'Bob Menendez', 'Cuban'),
+         race_checked = replace(race_checked, candidate == 'Dan Bongino', 'Italian'),
+         race_checked = replace(race_checked, candidate == "Al D'Amato", 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Lou Barletta', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Daniel Mongiardo', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Philip Giordano', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Rick Lazio', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Mark Ronchetti', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Pete Domenici', 'Italian'),
+         race_checked = replace(race_checked, candidate == 'Gloria Tristani', 'Latino_Hispanic'),
+         race_checked = replace(race_checked, candidate == 'Spencer Abraham', 'White')) %>%
   as.data.frame()
 
-sen_faces_clarifai <- sen_faces_clarifai %>%
-  mutate(clarifai_gender = replace(clarifai_gender, candidate == 'Misty K. Snow', 'feminine'),
-         clarifai_gender = replace(clarifai_gender, candidate == 'Slade Gorton', 'masculine'),
-         # 2020
-         clarifai_gender = replace(clarifai_gender, candidate == 'Ben Ray Lujan', 'masculine'),
-         clarifai_gender = replace(clarifai_gender, candidate == 'Dan Ahlers', 'masculine'),
-         clarifai_gender = replace(clarifai_gender, candidate == 'Lauren Witzke', 'feminine'),
-         clarifai_gender = replace(clarifai_gender, candidate == 'Merav Ben-David', 'feminine'),) %>%
-  as.data.frame()
-
-sen_faces_clarifai <- sen_faces_clarifai %>%
-  rename(gender_checked = clarifai_gender, race_checked = clarifai_race)
-
-write.csv(sen_faces_clarifai, "predictors_of_polling_errors/data/us_senate/race_gender_checked.csv")
+write.csv(sen_faces, 'predictors_of_polling_errors/data/us_senate/race_gender_checked.csv')
 
 # --------------------------------------------------------------------------- #
