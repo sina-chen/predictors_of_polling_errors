8391ea7eaa16ad5a60a6458e867f7704ac576411
Sina Chen <sina.chen@uni-konstanz.de>
2021-03-19 10:53:41 +0100

    update scrape code

--- german_landtag/scrape/scrape_wahlrecht_landtag.R
+++ german_landtag/scrape/scrape_wahlrecht_landtag.R
@@ -62,40 +62,11 @@ polls_lt_sample_size <- lapply(polls_lt_state,
                                function(x) lapply(x, function(y) clean_sample_size(y)))
 
 # add election year 
-polls_lt_election <- polls_election_long(polls_lt_sample_size)
+polls_lt_election <- add_election(polls_lt_sample_size)
 
 # clean institute
-polls_lt_election <- polls_lt_election %>% 
-  mutate(institute_clean = str_remove_all(institute, '\\s[*]|[*]') %>% 
-           tolower() %>% 
-           str_replace_all('\\s', '_'),
-         institute_clean = if_else(institute_clean == 'forschâ€™gr.wahlen' |
-                                  institute_clean == 'forschungs-gruppe_wahlen'|
-                                  institute_clean == 'forschungs-gruppewahlen'|
-                                  institute_clean == 'forschungsgruppewahlen' |
-                                  institute_clean == 'fgwtelefonfeld', 'fgw', institute_clean),
-         institute_clean = if_else(institute_clean == 'infratest_burke' |
-                                institute_clean == 'infratestburke'|
-                                institute_clean == 'infratestdimap'|
-                                institute_clean == 'infratestpolitik-forschung' |
-                                institute_clean == 'infratestpolitikforschung' |
-                                institute_clean == 'infratestsozialforschung'|
-                                institute_clean == 'tns_forschung'|
-                                institute_clean == 'tns_infratest'|
-                                institute_clean == 'tnsinfratest'|
-                                institute_clean == 'nfo_infratest', 'infratest',institute_clean),
-         institute_clean = if_else(institute_clean == 'gessphone_&_field', 'gess', institute_clean),
-         institute_clean = if_else(institute_clean == 'ifmleipzig', 'ifm_leipzig', institute_clean),
-         institute_clean = if_else(institute_clean == 'universitã¤tkiel', 'uni_kiel', institute_clean),
-         institute_clean = if_else(institute_clean == 'universitã¤thamburg', 'uni_hamburg', institute_clean),
-         institute_clean = if_else(institute_clean == 'polis', 'polis_sinus', institute_clean),
-         institute_clean = if_else(institute_clean == 'polis+sinus', 'polis_sinus', institute_clean),
-         institute_clean = if_else(institute_clean == 'mifmmã¼nchen', 'mifm_muenchen', institute_clean),
-         institute_clean = if_else(institute_clean == 'inra', 'ipsos', institute_clean)
-                                
-                                  )
-
-
+polls_lt_inst <- clean_institute(polls_lt_election)
+  
 
 #### Clean results ####
 
@@ -113,10 +84,9 @@ res_lt_info <- lapply(res_lt_oth,
 # add state identifier
 res_lt_state <- add_state(res_lt_info)
 
-# reshape to long and adjust party names
-res_lt_long <- party_names_long(res_lt_state)
-
+# adjust party names
+res_lt_name <- party_names(res_lt_state)
 
 # save results
-# saveRDS(polls_lt_election, 'data/landtag_polls.RDS')
-# saveRDS(res_lt_long, 'data/landtag_voteshare.RDS')
+saveRDS(polls_lt_election, 'data/landtag_polls_wide.RDS')
+saveRDS(res_lt_name, 'data/landtag_voteshare_wide.RDS')
