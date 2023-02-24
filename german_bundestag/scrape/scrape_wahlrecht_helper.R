bd82cbf06658fff1ddddf8be24dd7a41d9695ccb
Sina Chen <sina.chen@uni-konstanz.de>
2023-02-24 15:35:41 +0100

    update btw scrape

--- german_bundestag/scrape/scrape_wahlrecht_helper.R
+++ german_bundestag/scrape/scrape_wahlrecht_helper.R
@@ -16,6 +16,7 @@ get_institute_polls <- function(institute){
   # get html
   inst.url <- getURL(paste0("https://www.wahlrecht.de/umfragen/", institute, '.htm'))
 
+  # get individual year links
   if (institute != 'insa' & institute != 'yougov') {
     siteHtml2 <- htmlTreeParse(inst.url, useInternalNodes=TRUE)
     reg.year <- xpathSApply(siteHtml2, "//a/@href")
@@ -246,5 +247,115 @@ add_results <- function(polls, results) {
   return(df_results)
   
 }
+
+
+#### Get election results #####
+
+get_btw_results <- function(polls_raw){
+  results <- polls_raw[which(polls_raw$date_raw == 'Wahl 1998' | 
+                               polls_raw$sample_size == 'Bundestagswahl' |
+                               polls_raw$date == '27.09.1998'),] %>% 
+    mutate(election = str_extract(date_raw, '\\d{4}'),
+           result = str_remove_all(forecast, '%') %>% 
+             str_replace_all(',', '.') %>% 
+             str_replace_all('-|â€“|–', '0'),
+           result = if_else(str_detect(result, "FW") == T & election == 2021, "8.7", result)) %>% 
+    subset(str_detect(forecast, 'PIR') == F)  %>% 
+    select(-c(sample_size, period, date_raw, year, forecast, sonst_parties, 
+              poll_id)) %>% 
+    unique()  %>% 
+    mutate(result = as.numeric(result))
+
+  return(results)
+}
+
+#### Clean data #####
+
+clean_btw_polls <- function(polls_raw){
+  
+  # remove election results
+  clean_polls <- polls_raw[-which(polls_raw$date_raw == 'Wahl 1998' | 
+                                    polls_raw$sample_size == 'Bundestagswahl' |
+                                    polls_raw$date_raw == '27.09.1998'),]
+  
+  # sample_size, clean period, forecast and year
+  clean_polls <- clean_polls %>% 
+    mutate(sample_size = if_else(str_detect(sample_size, '[?]') == T, 
+                                 NA_character_, 
+                                 sample_size) %>%
+             str_remove_all('[^0-9]') %>% 
+             as.numeric(),
+           year = if_else(is.na(str_detect(date_raw, '\\d{4}')) == F & 
+                            year != str_extract(date_raw, '\\d{4}'),
+                          str_extract(date_raw, '\\d{4}'), year) %>% 
+             unlist(),
+           forecast = str_replace_all(forecast, '-|^[?]|–', NA_character_))
+  
+  # remove characters from forecast
+  clean_polls[which(str_detect(clean_polls$forecast, '[[:alpha:]]')),] <- clean_polls[which(str_detect(clean_polls$forecast, '[[:alpha:]]')),] %>% 
+    mutate(forecast = str_remove_all(forecast, '[^0-9]') %>% 
+             str_remove('^ ') %>% 
+             strsplit(split = "") %>%  
+             lapply(function(x) sum(as.numeric(x))) %>% 
+             unlist())
+  
+  # set empty cells to NA
+  clean_polls[clean_polls == ""] <- NA
+  
+  # convert date 
+  clean_polls <- clean_polls %>% 
+    mutate(date = as.Date(str_replace_all(date_raw, '[?][?]', '01'), '%d.%m.%Y'))
+  
+  # fill missing dates with field period information
+  clean_polls <- clean_polls %>% 
+    mutate(date = if_else(is.na(date) == T & is.na(period) == F, 
+                          as.Date(paste0(period,year), '%d.%m.–%d.%m.%Y'),
+                          date),
+           date_raw = date_raw %>% 
+             str_replace_all('Mrz', 'Mar') %>% 
+             str_replace_all('Okt', 'Oct') %>% 
+             str_replace_all('Dez', 'Dec') %>% 
+             str_replace_all('Mai', 'May.'))
+  
+  # insert date without information on day
+  clean_polls <- clean_polls %>%  
+    mutate(date = if_else(str_detect(date_raw, '\\w+[.] \\d+') == T & 
+                            is.na(date) == T,
+                          as.Date(as.yearmon(date_raw, '%b. %Y'), frac = 1), 
+                          date ))
+  
+  # add election and poll id
+  clean_polls <- clean_polls %>% 
+    mutate(election = case_when(date <= as.Date('27.09.1998', '%d.%m.%Y') |
+                                  date_raw == 'Wahl 1998' ~ 1998,
+                                date > as.Date('27.09.1998', '%d.%m.%Y') &
+                                  date <= as.Date('22.09.2002', '%d.%m.%Y') ~ 2002,
+                                date > as.Date('22.09.2002', '%d.%m.%Y') &
+                                  date <= as.Date('18.09.2005', '%d.%m.%Y') |
+                                  year == 2004 ~ 2005,
+                                date > as.Date('18.09.2005', '%d.%m.%Y') &
+                                  date <= as.Date('27.09.2009', '%d.%m.%Y') |
+                                  year == 2005 ~ 2009, # polls with missing information on date from emnid in 2005 were conducted after the election 2005 (source: wahlrecht)
+                                date > as.Date('27.09.2009', '%d.%m.%Y') &
+                                  date <= as.Date('22.09.2013', '%d.%m.%Y') ~ 2013,
+                                date > as.Date('22.09.2013', '%d.%m.%Y') &
+                                  date <= as.Date('24.09.2017', '%d.%m.%Y') ~ 2017,
+                                date > as.Date('24.09.2017', '%d.%m.%Y') &
+                                  date <= as.Date('26.09.2021', '%d.%m.%Y') ~ 2021,
+                                date > as.Date('26.09.2021', '%d.%m.%Y') ~ 2025),
+           poll_id = if_else(str_detect(poll_id, "NA") == T, 
+                             str_replace(poll_id, "NA", as.character(election)), 
+                             poll_id))
+  
+  
+  # remove NA forecast
+  clean_polls <- clean_polls %>% 
+    subset(is.na(forecast) == F | forecast != '0') %>% 
+    mutate(forecast = as.numeric(forecast)) %>% 
+    unique() %>% 
+    select(-c(date_raw, year)) 
+  
+  return(clean_polls)
+} 
 
 
